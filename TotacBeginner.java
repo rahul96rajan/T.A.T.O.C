6a8447f0eedce6b086707015f7e6b55d1dd9fe1c
Rahul Rajan <40260491+rahul96rajan@users.noreply.github.com>
2018-06-17 19:18:08 +0530

    Update TotacBeginner.java

--- TotacBeginner.java
+++ TotacBeginner.java
@@ -3,11 +3,14 @@ package tatoc;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class Totac {
+public class TatocBeginner {
 	public static void main (String args[]){
+		
 		WebDriver driver = new FirefoxDriver();
 		
 		driver.get("http://10.0.1.86/tatoc");
+		
 		//driver.findElement(By.className("greenbox")).click();
+		
 	}
 }
