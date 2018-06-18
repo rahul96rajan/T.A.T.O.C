4a8eeb0ac4936f28b096e31d5e07db623951dac3
Rahul Rajan <40260491+rahul96rajan@users.noreply.github.com>
2018-06-18 22:49:41 +0530

    Update TatocBeginner.java

--- TatocBeginner.java
+++ TatocBeginner.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class Totac {
 	public static void main (String args[]){
